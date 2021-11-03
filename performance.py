9b876c2cb459892d1ad459f6cd41a1c67884c33d
janeyeon <khy5630@snu.ac.kr>
2021-11-03 18:15:12 +0900

    :building_construction: performance.py 에 가져오게 만듦

--- performance.py
+++ performance.py
@@ -1,13 +1,86 @@
 import qiskit
-from qiskit import QuantumCircuit
-from qiskit import Aer, execute
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit import Aer, transpile, assemble
 import random
-import math
 import numpy as np
-import matplotlib.pyplot as plt
-import dataExtraction as de
-import simulator as sim
 from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+from vqls import vqls_calculate_cost_function, apply_fixed_ansatz
+from vqls import coefficient_set as vqls_coefficient_set
 # import the circuit 
+
+#뽑아주고자 했던 변수를 앞에서 미리 정의한다 
+iter = 5
+# function for plot
+class OpObj(object):
+    def __init__(self):
+        #맨처음 params의 초기값 
+        self.x_0 = [float(random.randint(0,3000))/1000 for i in range(0, 9)]
+        #minimize 중간에 뽑아져 나오는 값을 담을 변수
+        self.f = np.full(shape=(iter,), fill_value=np.NaN)
+        #그걸 f 의 몇번째에 넣을지 계산하는 변수 
+        self.count = 0
+
+#뽑아진 obj_fun의 값에다가 중간 param을 넣어서 계산할 값임 
+def callback(xk, obj=None):
+    obj.f[obj.count] = xk
+    obj.count += 1
+
+
+# fig, ax = plt.subplots(1,1)
+x = np.linspace(1,iter, iter)
+
+#====================== for vqls ======================
+vqls_op_obj = OpObj()
+
+# cost function 을 minimize 하는 parameter alpha 값을 구한다 (9개)
+
+vqls_out = minimize(vqls_calculate_cost_function, x0= vqls_op_obj.x_0, args=(vqls_op_obj, callback), options={'maxiter':iter}, method="COBYLA")
+
+plt.plot(x, vqls_op_obj.f)
+
+# 여기서 부터는 구한 결과로 비교하는 부분 
+
+# 구한 output 을 쪼갠다  
+# 아직 out을 구하지 않은 상황 -> 넣어주어야 함 
+out_f = [vqls_out['x'][0:3], vqls_out['x'][3:6], vqls_out['x'][6:9]]
+
+# min parameter 을 이용해서 다시 한번 ansatz 를 계산한다 
+circ = QuantumCircuit(3, 3)
+apply_fixed_ansatz([0, 1, 2], out_f)
+circ.save_statevector()
+
+backend = Aer.get_backend('aer_simulator')
+
+t_circ = transpile(circ, backend)
+qobj = assemble(t_circ)
+job = backend.run(qobj)
+
+result = job.result()
+# 그걸로 확률을 구한다 = |x> 
+o = result.get_statevector(circ, decimals=10)
+
+#얘가 실제 coeff 값을 토대로 만든 matrix A = 0.55I + 0.225Z_2 + 0.225Z_3
+a1 = vqls_coefficient_set[2]*np.array([[1,0,0,0,0,0,0,0], [0,1,0,0,0,0,0,0], [0,0,1,0,0,0,0,0], [0,0,0,1,0,0,0,0], [0,0,0,0,-1,0,0,0], [0,0,0,0,0,-1,0,0], [0,0,0,0,0,0,-1,0], [0,0,0,0,0,0,0,-1]])
+a0 = vqls_coefficient_set[1]*np.array([[1,0,0,0,0,0,0,0], [0,1,0,0,0,0,0,0], [0,0,-1,0,0,0,0,0], [0,0,0,-1,0,0,0,0], [0,0,0,0,1,0,0,0], [0,0,0,0,0,1,0,0], [0,0,0,0,0,0,-1,0], [0,0,0,0,0,0,0,-1]])
+a2 = vqls_coefficient_set[0]*np.array([[1,0,0,0,0,0,0,0], [0,1,0,0,0,0,0,0], [0,0,1,0,0,0,0,0], [0,0,0,1,0,0,0,0], [0,0,0,0,1,0,0,0], [0,0,0,0,0,1,0,0], [0,0,0,0,0,0,1,0], [0,0,0,0,0,0,0,1]])
+#이게 진짜 최종 matrix A 
+a3 = np.add(np.add(a2, a0), a1)
+#실제 b의 값 
+#이친구 ||b|| 의 값이 1임 ㅋㅋㅋ 
+b = np.array([float(1/np.sqrt(8)),float(1/np.sqrt(8)),float(1/np.sqrt(8)),float(1/np.sqrt(8)),float(1/np.sqrt(8)),float(1/np.sqrt(8)),float(1/np.sqrt(8)),float(1/np.sqrt(8))])
+
+#결과 값이 1에 가까울수록 좋음 
+# (b * (b' / ||b'||))^2 
+print("vqls result: ", (b.dot(a3.dot(o)/(np.linalg.norm(a3.dot(o)))))**2)
+
+
+
+#=========== plot =============
+plt.show()
+
+
+
+
 
 
--- vqls.py
+++ vqls.py
@@ -6,36 +6,9 @@ import random
 import numpy as np
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
-from functools import partial
 
-#뽑아주고자 했던 변수를 앞에서 미리 정의한다 
-iter = 5
-# function for plot
-class OpObj(object):
-    def __init__(self):
-        #맨처음 params의 초기값 
-        self.x_0 = [float(random.randint(0,3000))/1000 for i in range(0, 9)]
-        #minimize 중간에 뽑아져 나오는 값을 담을 변수
-        self.f = np.full(shape=(iter,), fill_value=np.NaN)
-        #그걸 f 의 몇번째에 넣을지 계산하는 변수 
-        self.count = 0
-
-    def _fun(self, param):
-        #여기에 계산된 param: (xk)의 값을 집어넣는다
-        return calculate_cost_function(param)
-
-#뽑아진 obj_fun의 값에다가 중간 param을 넣어서 계산할 값임 
-def cb(xk, obj=None):
-    obj.f[obj.count] = xk
-    obj.count += 1
-
-
-# fig, ax = plt.subplots(1,1)
-x = np.linspace(1,iter, iter)
-
-op_obj = OpObj()
-
-
+coefficient_set = [0.55, 0.225, 0.225]
+gate_set = [[0, 0, 0], [0, 1, 0], [0, 0, 1]]
 
 def apply_fixed_ansatz(qubits, parameters):
 
@@ -54,9 +27,6 @@ def apply_fixed_ansatz(qubits, parameters):
     for iz in range (0, len(qubits)):
         circ.ry(parameters[2][iz], qubits[iz])
 
-# circ = QuantumCircuit(3)
-# apply_fixed_ansatz([0, 1, 2], [[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-
 # Creates the Hadamard test
 
 def had_test(gate_type, qubits, auxiliary_index, parameters):
@@ -74,9 +44,6 @@ def had_test(gate_type, qubits, auxiliary_index, parameters):
             circ.cz(auxiliary_index, qubits[ie])
     
     circ.h(auxiliary_index)
-    
-# circ = QuantumCircuit(4)
-# had_test([[0, 0, 0], [0, 0, 1]], [1, 2, 3], 0, [[1, 1, 1], [1, 1, 1], [1, 1, 1]])
 
 # Creates controlled anstaz for calculating |<b|psi>|^2 with a Hadamard test
 
@@ -107,17 +74,12 @@ def control_fixed_ansatz(qubits, parameters, auxiliary, reg):
     for i in range (0, len(qubits)):
         circ.cry(parameters[2][i], qiskit.circuit.Qubit(reg, auxiliary), qiskit.circuit.Qubit(reg, qubits[i]))
 
-# q_reg = QuantumRegister(5)
-# circ = QuantumCircuit(q_reg)
-# control_fixed_ansatz([1, 2, 3], [[1, 1, 1], [1, 1, 1], [1, 1, 1]], 0, q_reg)
 
 def control_b(auxiliary, qubits):
 
     for ia in qubits:
         circ.ch(auxiliary, ia)
 
-# circ = QuantumCircuit(4)
-# control_b(0, [1, 2, 3])
 
 # Create the controlled Hadamard test, for calculating <psi|psi>
 
@@ -146,7 +108,7 @@ def special_had_test(gate_type, qubits, auxiliary_index, parameters, reg):
 
 # Implements the entire cost function on the quantum circuit
 
-def calculate_cost_function(parameters):
+def vqls_calculate_cost_function(parameters, vqls_op_obj, callback):
     
     global opt
 
@@ -227,28 +189,23 @@ def calculate_cost_function(parameters):
 
     print(1-float(overall_sum_2/overall_sum_1))
     # 이제 이 값을 op_obj.f 에 넣어준다 
-    cb(xk= 1-float(overall_sum_2/overall_sum_1), obj=op_obj)
+    callback(xk= 1-float(overall_sum_2/overall_sum_1), obj=vqls_op_obj)
 
     return 1-float(overall_sum_2/overall_sum_1)
 
-coefficient_set = [0.55, 0.225, 0.225]
-gate_set = [[0, 0, 0], [0, 1, 0], [0, 0, 1]]
 
-# cost function 을 minimize 하는 parameter alpha 값을 구한다 (9개)
 
-x_soln = minimize(calculate_cost_function, x0= op_obj.x_0, options={'maxiter':iter}, method="COBYLA")
+# # cost function 을 minimize 하는 parameter alpha 값을 구한다 (9개)
 
-print("op_obj.f ", op_obj.f)
-print("x", x)
-plt.plot(x, op_obj.f)
-plt.show()
+# out = minimize(vqls_calculate_cost_function, x0= op_obj.x_0, args=(op_obj, callback), options={'maxiter':iter}, method="COBYLA")
 
-# minimize(calculate_cost_function, x0=[float(random.randint(0,3000))/1000 for i in range(0, 9)], method="COBYLA", options={'maxiter':200})
+# plt.plot(x, op_obj.f)
+# plt.show()
 
-## 여기서 부터는 구한 결과로 비교하는 부분 
+# # 여기서 부터는 구한 결과로 비교하는 부분 
 
-# 구한 output 을 쪼갠다  
-# 아직 out을 구하지 않은 상황 -> 넣어주어야 함 
+# # 구한 output 을 쪼갠다  
+# # 아직 out을 구하지 않은 상황 -> 넣어주어야 함 
 # out_f = [out['x'][0:3], out['x'][3:6], out['x'][6:9]]
 
 # # min parameter 을 이용해서 다시 한번 ansatz 를 계산한다 
